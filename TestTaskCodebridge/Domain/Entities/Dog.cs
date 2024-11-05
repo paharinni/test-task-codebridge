093ede96f8a815b0c8407ec0115990d9133dc414
paharinni <pakharenkobusiness@gmail.com>
2024-11-06 01:02:26 +0200

    Added entities

---
+++ TestTaskCodebridge/Domain/Entities/Dog.cs
@@ -0,0 +1,10 @@
+namespace TestTaskCodebridge.Domain.Entities;
+
+public class Dog
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Color { get; set; }
+    public int TailLength { get; set; }
+    public int Weight { get; set; }
+}
