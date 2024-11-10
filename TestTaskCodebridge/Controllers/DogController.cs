7d60ec99998b946b3cbe2d0c5b25e714daed1735
paharinni <pakharenkobusiness@gmail.com>
2024-11-11 01:34:19 +0200

    made http methods asynchronous

--- TestTaskCodebridge/Controllers/DogController.cs
+++ TestTaskCodebridge/Controllers/DogController.cs
@@ -19,19 +19,19 @@ public class DogController : ControllerBase
     }
 
     [HttpGet]
-    public IActionResult GetAll()
+    public async Task<IActionResult> GetAll()
     {
-        var dogs = _context.Dogs
+        var dogs = await _context.Dogs
             .Select(d => d.ToDogDto())
-            .ToList();
+            .ToListAsync();
 
         return Ok(dogs);
     }
     
     [HttpGet("{id}")]
-    public IActionResult GetById([FromRoute] int id)
+    public async Task<IActionResult> GetById([FromRoute] int id)
     {
-        var dog = _context.Dogs.Find(id);
+        var dog = await _context.Dogs.FindAsync(id);
 
         if (dog == null)
         {
@@ -42,12 +42,50 @@ public class DogController : ControllerBase
     }
     
     [HttpPost]
-    public IActionResult Create([FromBody] CreateDogRequestDto dogDto)
+    public async Task<IActionResult> Create([FromBody] CreateDogRequestDto dogDto)
     {
         var dogModel = dogDto.ToDogFromCreateDto();
-        _context.Dogs.Add(dogModel);
-        _context.SaveChanges();
+        await _context.Dogs.AddAsync(dogModel);
+        await _context.SaveChangesAsync();
     
         return CreatedAtAction(nameof(GetById), new { id = dogModel.Id }, dogModel.ToDogDto());
+    }
+
+    [HttpPut]
+    [Route("{id}")]
+    public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateDogRequestDto updateDto)
+    {
+        var dogModel = await _context.Dogs.FirstOrDefaultAsync(x => x.Id == id);
+        
+        if (dogModel == null)
+        {
+            return NotFound();
+        }
+
+        dogModel.Name = updateDto.Name;
+        dogModel.Color = updateDto.Color;
+        dogModel.Weight = updateDto.Weight;
+        dogModel.TailLength = updateDto.TailLength;
+
+        await _context.SaveChangesAsync();
+
+        return Ok(dogModel.ToDogDto());
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    public async Task<IActionResult> Delete([FromRoute] int id)
+    {
+        var dogModel = await _context.Dogs.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (dogModel == null)
+        {
+            return NotFound();
+        }
+
+        _context.Dogs.Remove(dogModel);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
     }
 }
---
+++ TestTaskCodebridge/Domain/Dtos/UpdateDogRequestDto.cs
@@ -0,0 +1,9 @@
+namespace TestTaskCodebridge.Domain.Dtos;
+
+public class UpdateDogRequestDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string Color { get; set; } = string.Empty;
+    public int TailLength { get; set; }
+    public int Weight { get; set; }
+}
